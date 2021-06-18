a5fb3dd7ae9d13d55cbd7e5b459445c3fdf0ed3b
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-06-18 08:55:26 +0530

    new script activation

---
+++ templates/devcontainer/{{cookiecutter.devcontainer_folder_name}}/scripts/base.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# setup the git identities
+git config --global user.name "${GIT_AUTHOR_NAME}"
+git config --global user.email "${GIT_AUTHOR_EMAIL}"
+
+# remove the default fish shell prompt
+set fish_greeting
+
+# Install the dependencies of each micro-service
+PROJECT_FOLDER="/workspace/{{ cookiecutter.project_name }}"
+cd "$PROJECT_FOLDER"
+
+for filename in $PROJECT_FOLDER/.devcontainer/scripts/private-*; do
+    [ -e "$filename" ] || continue
+    source "$filename"
+done
