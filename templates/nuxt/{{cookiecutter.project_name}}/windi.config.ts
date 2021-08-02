5b2203a4ca374c5322f624b88babf26ab6a77ae4
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-08-02 09:07:40 +0530

    update windi config

--- templates/nuxt/{{cookiecutter.project_name}}/windi.config.ts
+++ templates/nuxt/{{cookiecutter.project_name}}/windi.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'windicss/helpers'
 import defaultTheme from 'windicss/defaultTheme'
 import colors from 'windicss/colors'
 
+const PRODUCTION = process.env.NODE_ENV !== 'production'
+
 export default defineConfig({
   extract: {
     include: ['**/*.{vue,html,jsx,tsx}'],
@@ -50,5 +52,10 @@ export default defineConfig({
         spacing: 'margin, padding',
       },
     },
+  },
+  shortcuts: {
+    'debug-screens': PRODUCTION
+      ? 'before:bottom-0 before:left-0 before:fixed before:z-[2147483647] before:px-1 before:text-12px before:bg-black before:text-white before:shadow-xl @sm:before:content-["screen:sm"] @md:before:content-["screen:md"] @lg:before:content-["screen:lg"] @xl:before:content-["screen:xl"] @2xl:before:content-["screen:2xl"] <sm:before:content-["screen:none"]'
+      : '',
   },
 })
