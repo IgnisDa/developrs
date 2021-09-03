fd6a967117ff632d9d45075717de3254534324b5
Diptesh Choudhuri <ignisda2001@gmail.com>
2021-09-03 20:44:06 +0530

    update devcontainer

--- templates/devcontainer/{{cookiecutter.devcontainer_folder_name}}/scripts/base.sh
+++ templates/devcontainer/{{cookiecutter.devcontainer_folder_name}}/scripts/base.sh
@@ -7,11 +7,14 @@ git config --global user.email "${GIT_AUTHOR_EMAIL}"
 # remove the default fish shell prompt
 set fish_greeting
 
+# own the HOME cache directory
+sudo chown -R $USERNAME:$USERNAME $HOME/.cache
+
 # Install the dependencies of each micro-service
 PROJECT_FOLDER="/workspace/{{ cookiecutter.project_name }}"
 cd "$PROJECT_FOLDER"
 
-for filename in $PROJECT_FOLDER/.devcontainer/scripts/private-*; do
+for filename in $PROJECT_FOLDER/{{ cookiecutter.devcontainer_folder_name }}/scripts/private-*; do
     [ -e "$filename" ] || continue
     . "$filename"
 done
