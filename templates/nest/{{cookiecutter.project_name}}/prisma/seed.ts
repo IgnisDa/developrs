c949945ad823d0f50c7fdbf69ef2901b46f6a3f3
IgnisDa <ignisda2001@gmail.com>
2021-09-12 12:11:10 +0530

    add default user in seed

--- templates/nest/{{cookiecutter.project_name}}/prisma/seed.ts
+++ templates/nest/{{cookiecutter.project_name}}/prisma/seed.ts
@@ -2,13 +2,25 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-async function seed() { }
+async function seed() {
+  await prisma.user.upsert({
+    where: {
+      username: 'admin',
+    },
+    update: {},
+    create: {
+      username: 'admin',
+      email: 'admin@email.com',
+      password: 'admin',
+    },
+  });
+}
 
 seed()
-    .catch((e) => {
-        console.error(e);
-        process.exit(1);
-    })
-    .finally(async () => {
-        await prisma.$disconnect();
-    });
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
