36e9e223172916e6e67a1b5c93bcd850ff581033
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-01-19 02:38:17 +0000

    ci: fix init scripts

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -9,6 +9,7 @@ set fish_greeting
 
 # Install the dependencies of each micro-service
 cd "$PROJECT_FOLDER"
+cargo build
 
 for filename in $PROJECT_FOLDER/.devcontainer/scripts/private-*; do
     [ -e "$filename" ] || continue
