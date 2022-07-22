b3a3ef6b7f50c35b83beca54b642680338fca0e7
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-07-22 16:08:15 +0000

    ci: add devcontainer settings for node

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -5,14 +5,4 @@ git config --global user.name "${GIT_AUTHOR_NAME}"
 git config --global user.email "${GIT_AUTHOR_EMAIL}"
 
 # remove the default fish shell prompt
-sudo pip install httpie
-set fish_greeting
-
-# Install the dependencies of each micro-service
-cd "$PROJECT_FOLDER"
-cargo build
-
-for filename in $PROJECT_FOLDER/.devcontainer/scripts/private-*; do
-    [ -e "$filename" ] || continue
-    . "$filename"
-done
+pip3 install --user httpie
