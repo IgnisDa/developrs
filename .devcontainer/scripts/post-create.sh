fbe4ce94d05d74968fa68930ed1d034667644ee9
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-10-11 01:41:37 +0000

    ci: use archlinux for devcontainer image

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -3,6 +3,3 @@
 # setup the git identities
 git config --global user.name "${GIT_AUTHOR_NAME}"
 git config --global user.email "${GIT_AUTHOR_EMAIL}"
-
-# remove the default fish shell prompt
-pip3 install --user httpie
