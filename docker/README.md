589f510a9ddb44c0adc0f1fb1a4d1d1c2fc60f0b
Diptesh <ignisda2001@gmail.com>
2023-01-09 11:23:52 +0530

    feat(docker): install sshd

--- docker/README.md
+++ docker/README.md
@@ -8,5 +8,5 @@ development environment.
 To build the image, execute the following command in this directory:
 
 ```bash
-docker build --build-arg BUILD_DATE=(date -u +'%Y-%m-%dT') -t ignisda/archlinux:latest .
+docker build --build-arg BUILD_DATE=(date -u +'%Y-%m-%dT') --file docker/Dockerfile --tag ignisda/archlinux:latest .
 ```
---
+++ docker/init-ssh.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -uex
+
+SSHD_PORT=2222
+echo "${USERNAME}:password" | chpasswd
+
+pacman -S --noconfirm openssh lsof
+groupadd ssh
+usermod -aG ssh "${USERNAME}"
+
+# Setup sshd
+mkdir -p /var/run/sshd
+sed -i 's/session\s*required\s*pam_loginuid\.so/session optional pam_loginuid.so/g' /etc/pam.d/sshd
+sed -i 's/#*PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sed -i -E "s/#*\s*Port\s+.+/Port ${SSHD_PORT}/g" /etc/ssh/sshd_config
+# Need to UsePAM so /etc/environment is processed
+sed -i -E "s/#?\s*UsePAM\s+.+/UsePAM yes/g" /etc/ssh/sshd_config
+
+tee -a /usr/local/share/ssh-init.sh > /dev/null \
+<< 'EOF'
+#!/usr/bin/env bash
+
+set -e 
+
+# ** Start SSH server **
+/usr/bin/sshd -o ListenAddress=0.0.0.0 2>&1 | tee /tmp/sshd.log > /dev/null
+set +e
+exec "$@"
+EOF
+chmod +x /usr/local/share/ssh-init.sh
+
+#ssh-keygen -A
+#/usr/local/share/ssh-init.sh
